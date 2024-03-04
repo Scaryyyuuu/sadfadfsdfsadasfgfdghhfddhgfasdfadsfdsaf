6b27f1eaba3d76902569403d1f25e64f33689bb9
Scaryyyuuu <162179909+Scaryyyuuu@users.noreply.github.com>
2024-03-04 20:46:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";//istediğiniz gibi ayarlayabilirsiniz prefixi
-var statuses = [`Altyapı Videosu İçin youtube.com/@WraithsDev`];
+var statuses = [`Çekiliş Duyuru`];
 var timers = 2;
-const owners = ["sahip_id"];//botu kullanmak için sahip_id girmeyi unutmayın
+const owners = ["628244379936620546"];//botu kullanmak için sahip_id girmeyi unutmayın
 
 client.on("ready", () => {
   console.log(`Giriş Yapıldı: ${client.user.tag}`);
@@ -157,4 +157,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(`bottokengir`);
+client.login(`MTIwODM3NTQ2NzM5MzE1NTA4Mg.GBToXK.SP0yzPWGBWhjuaxL6Vw2OSCDQ1gqFr7Fjq3ii8`);
